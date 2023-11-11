2b92f3a777314d0c046966394e27306d419f126c
alv18051 <alv18051@uvg.edu.gt>
2023-11-10 21:59:06 -0600

    agregar logica para enviar datos cada 15 segundos

--- lab9.py
+++ lab9.py
@@ -1,6 +1,8 @@
 import json
 import random
 import numpy as np
+import time
+from confluent_kafka import Producer
 
 def generate_sensor_data():
     temperature = np.clip(np.random.normal(50, 10), 0, 100)
@@ -11,15 +13,37 @@ def generate_sensor_data():
         "temperatura": round(temperature, 2), "humedad": relative_humidity, "direccion_viento": wind_direction
     }
 
-def generate_and_save_json_data(num_records, filename):
-    data = [generate_sensor_data() for _ in range(num_records)]
+# def generate_and_save_json_data(num_records, filename):
+#     data = [generate_sensor_data() for _ in range(num_records)]
     
-    with open(filename, 'w') as file:
-        json_data = '[' + ',\n'.join(json.dumps(record) for record in data) + ']'
-        file.write(json_data)
+#     with open(filename, 'w') as file:
+#         json_data = '[' + ',\n'.join(json.dumps(record) for record in data) + ']'
+#         file.write(json_data)
 
+conf = {
+    'bootstrap.servers': '157.245.244.105:9092',
+    'client.id': 'sensor-data-producer'
+}
 
+def delivery_report(err, msg):
+    if err is not None:
+        print('Message delivery failed: {}'.format(err))
+    else:
+        print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
 
-generate_and_save_json_data(50, 'sensor_data.json')
+def produce_sensor_data(interval):
+    try:
+        while True:
+            data = generate_sensor_data()
+            producer.produce('18051', json.dumps(data), callback=delivery_report)
+            producer.poll(0)
+            time.sleep(interval)
+    except KeyboardInterrupt:
+        print("Data transmission stopped by user.")
+        producer.flush()
+
+producer = Producer(conf)
+produce_sensor_data(15)
+
 
 
