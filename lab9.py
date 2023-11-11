c07c8a7958a40ce63480e7a2ebac5d2b0a229ecb
alv18051 <alv18051@uvg.edu.gt>
2023-11-10 22:07:37 -0600

    ride the lighting

--- lab9.py
+++ lab9.py
@@ -21,8 +21,14 @@ def generate_sensor_data():
 #         file.write(json_data)
 
 conf = { #configuración de kafka
-    'bootstrap.servers': '157.245.244.105:9092',
-    'client.id': 'sensor-data-producer'
+    #'bootstrap.servers': '157.245.244.105:9092',
+    'bootstrap.servers': 'lab9.alumchat.xyz:9092',
+    'client.id': 'sensor-data-producer',
+    'linger.ms': 1,
+    'acks': 1,
+    'retries': 5,
+    'retry.backoff.ms': 1000
+
 }
 
 def delivery_report(err, msg):
@@ -39,8 +45,11 @@ def produce_sensor_data(interval):
             producer.poll(0)
             time.sleep(interval)
     except KeyboardInterrupt:
-        print("Data transmission stopped by user.")
+        print("Data transmission interrumpida.")
+    finally:
+        print("Flushing queue...")
         producer.flush()
+        print("Bye.")
 
 producer = Producer(conf)
 produce_sensor_data(15)#cambiar si se desea más de 15 segundos o menos
